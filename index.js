ecc88040cf9e5816c72b3b19dabb3de2ea525f2e
Aman Murya <m.amanmaurya49@gmail.com>
2023-03-30 15:35:27 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express=require('express')
+const path=require('path')
+const app=express();
+const PORT=process.env.PORT || 4000;
+const server=app.listen(PORT || 4000,()=>{
+    console.log(`server is running on localhost:${PORT}`)
+})
+
+app.use(express.static(path.join(__dirname,'public')))
